5d273f31f95532af5e8e12647b7efcc31b761050
Gyges <shepherd2sovereign@gmail.com>
2026-05-15 11:57:16 -0600

    some progress

--- src/bin/kuva/parquet.rs
+++ src/bin/kuva/parquet.rs
@@ -1,4 +1,6 @@
-use arrow::array::RecordBatch;
+use arrow::array::{RecordBatch, RecordBatchReader};
+use arrow::compute::cast;
+use arrow::datatypes::DataType;
 use bytes::Bytes;
 use parquet::arrow::ProjectionMask;
 use parquet::schema::types::{SchemaDescriptor, Type};
@@ -12,8 +14,10 @@ use arrow::record_batch;
 use parquet::arrow::arrow_reader::ParquetRecordBatchReaderBuilder;
 
 pub struct ParquetScatterSource {
-    pub x_col: record_batch::RecordBatch,
-    pub y_cols: record_batch::RecordBatch,
+    // While x axis could be a non-numeric type, in bin/kuva/scatter.rs,
+    // CLI Scatter only supports numeric X axis anyways
+    pub x_col: Vec<f64>,
+    pub y_cols: Vec<Vec<f64>>,
 }
 
 impl ParquetScatterSource {
@@ -28,11 +32,11 @@ impl ParquetScatterSource {
         // Important: to enter this function, the data should have already been validated to be parquet
         // @dev TODO: how to validate that stdin is parquet. ===
         input: Option<&Path>,
-        x: ColSpec,
-        y: Vec<ColSpec>,
+        x_col: ColSpec,
+        y_cols: Vec<ColSpec>,
     ) -> Result<Self, String> {
-        // Logic for reading parquet data as bytes from stdin
-
+        // we need different branches for stdin, which uses a SyncReader<bytes>, and
+        // from file, which uses a SyncReader<file>
         match input {
             Some(p) if p.to_str() != Some("-") => {
                 let file = File::open(input.unwrap())
@@ -40,15 +44,67 @@ impl ParquetScatterSource {
                 let mut builder = ParquetRecordBatchReaderBuilder::try_new(file)
                     .map_err(|e| format!("Failed to read from the provided parquet file. {}", e))?;
 
-                let schema = builder.parquet_schema();
+                // Lazily identify only the columns we want
+                let mask = {
+                    let schema = builder.parquet_schema();
 
-                let x_leaf_idx: usize = parquet_schema_match_colspec(&x, schema)?;
-                let y_leaf_idxs: Vec<usize> = y
-                    .iter()
-                    .map(|y| parquet_schema_match_colspec(y, schema))
-                    .collect::<Result<Vec<_>, _>>()?;
+                    let x_leaf_idx: usize = resolve_colspec(&x_col, schema)?;
+                    let y_leaf_idxs: Vec<usize> = y_cols
+                        .iter()
+                        .map(|y| resolve_colspec(y, schema))
+                        .collect::<Result<Vec<_>, _>>()?;
+                    let all_leaves: Vec<usize> = std::iter::once(x_leaf_idx)
+                        .chain(y_leaf_idxs.into_iter())
+                        .collect();
 
-                // TODO: Construct the actual ParquetScatterSource, and include a ProjectionMask
+                    ProjectionMask::leaves(schema, all_leaves)
+                };
+
+                let reader = builder
+                    .with_projection(mask)
+                    .build()
+                    .map_err(|e| format!("Failed to construct parquet reader. {}", e))?;
+
+                // Check that each column is a supported (i.e., numeric) datatype
+                let schema = reader.schema();
+
+                for field in schema.fields() {
+                    match field.data_type() {
+                        DataType::Int8
+                        | DataType::Int16
+                        | DataType::Int32
+                        | DataType::Int64
+                        | DataType::UInt8
+                        | DataType::UInt16
+                        | DataType::UInt32
+                        | DataType::UInt64
+                        | DataType::Float16
+                        | DataType::Float32
+                        | DataType::Float64 => {}
+                        _ => {
+                            return Err(format!("Unsupported datatype: {:?}", field.data_type()));
+                        }
+                    }
+                }
+
+                let mut x_vec: Vec<f64> = Vec::new();
+                let mut y_vec: Vec<Vec<f64>> = vec![Vec::new(); y_cols.len()];
+
+                // Parquets are read in batches of row, so one batch is a 2d data shape
+                // where the columns are defined by the projection mask
+                for batch in reader {
+                    let batch = batch.map_err(|e| format!("Failed to read parquet batch: {e}"))?;
+                    let x_col = batch.column(0);
+                    let casted = cast(x_col, &DataType::Float64)
+                        .map_err(|e| format!("Failed to cast column to Float64. {}", e))?;
+                    // you are here: 5/15
+                    // let float64 = casted.as_any().downcast_ref::<Float64Array>().ok_or(...)?;
+                    // let values: Vec<f64> = ...
+
+                    for y_col in batch.columns().iter() {
+                        let values = y_col;
+                    }
+                }
             }
             _ => {
                 let mut buf = Vec::new();
@@ -60,10 +116,10 @@ impl ParquetScatterSource {
 
                 let schema = builder.parquet_schema();
 
-                let x_leaf_idx: usize = parquet_schema_match_colspec(&x, schema)?;
-                let y_leaf_idxs: Vec<usize> = y
+                let x_leaf_idx: usize = resolve_colspec(&x_col, schema)?;
+                let y_leaf_idxs: Vec<usize> = y_cols
                     .iter()
-                    .map(|y| parquet_schema_match_colspec(y, schema))
+                    .map(|y| resolve_colspec(y, schema))
                     .collect::<Result<Vec<_>, _>>()?;
 
                 // TODO: Construct the actual ParquetScatterSource, and include a ProjectionMask
@@ -72,13 +128,13 @@ impl ParquetScatterSource {
 
         // @dev TODO: delete this placeholder
         return Ok(ParquetScatterSource {
-            x_col: RecordBatch::new_empty(std::sync::Arc::new(arrow::datatypes::Schema::empty())),
-            y_cols: RecordBatch::new_empty(std::sync::Arc::new(arrow::datatypes::Schema::empty())),
+            x_col: vec![0.0],
+            y_cols: vec![vec![0.0], vec![0.0]],
         });
     }
 }
 
-fn parquet_schema_match_colspec(col: &ColSpec, schema: &SchemaDescriptor) -> Result<usize, String> {
+fn resolve_colspec(col: &ColSpec, schema: &SchemaDescriptor) -> Result<usize, String> {
     match col {
         ColSpec::Index(i) => {
             let all_roots = schema.root_schema().get_fields();
